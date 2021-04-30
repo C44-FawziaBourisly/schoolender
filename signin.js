29514c38c84c30b168f827f20e1c92d08a223ede
saraallahdan-C43 <66418277+saraallahdan-C43@users.noreply.github.com>
2021-04-30 23:58:12 +0300

    dd

--- signin.js
+++ signin.js
@@ -34,7 +34,91 @@ document.querySelector('.signupbtn').addEventListener('click', (e) => {
     console.log(cred.user) 
     alert(`done`);
      window.location.href = "./index.html";
+
+
    
 });
 
- }) 
+firebase.auth().signInWithEmailAndPassword(inputva, inputvla).catch(function(error){
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMsg = error.message;
+    // [START_EXCLUDE]
+    if (errorCode ==='auth/wrong-password'){
+     alert('Wrong password.');
+    } else {
+     alert(errorMsg);
+    }
+    console.log(error);
+    document.getElementById('btnLogin').disabled = false;
+   });
+   // [END authwithemail]
+   //disables the button after logging in
+   document.getElementById('btnLogin').disabled = true;  
+  
+   const logoutBtn = document.getElementById('#login-email');
+  const logoutBtn = document.querySelector('#logout-btn');
+   logoutBtn.addEventListener('click', e => {
+     e.preventDefault();
+    
+     firebase.auth().signOut()
+     .then(function() {
+        alert('Sign-out successful.');
+       // Sign-out successful.
+     })
+     .catch(function(error) {
+       // An error happened
+       alert('An error happened')
+     });
+
+
+   }) 
+}
+// (function(){//initialize the firebase app
+//     var config = {
+                  
+//         }; 
+
+//         firebase.initializeApp(config);
+//         var auth = null;
+//         var loginBtn= document.getElementById('btnLogin');
+//         firebase.auth.Auth.Persistence.SESSION;
+
+//     //Login
+//     loginBtn.addEventListener('click', e => {
+//         e.preventDefault();
+//         if( $('#email').val() != '' && $('#password').val() != '' ){
+//         //login the user
+//         var data = {
+//             email: $('#email').val(),
+//             password: $('#password').val()
+//         };
+
+//         firebase.auth().signInWithEmailAndPassword(data.email, data.password)
+//             .then(function(authData) {
+//             auth = authData;
+//             })
+//             .catch(function(error) {
+//             console.log("Login Failed!", error);
+//             window.alert("Login Failed!", error);
+//             });
+//         }
+//     });
+// })();
+
+
+// function check(){
+// if(document.getElementById("victory").value == "victory")
+//     return true;
+// else 
+//    return false;
+// firebase.auth().onAuthStateChanged(function(user){
+//     if(user){
+//         window.location.href = "patientDashboard.html";
+//         console.log(user);
+//     }
+//     else{
+//         console.log('no user present');
+//     }
+// })
+ )
